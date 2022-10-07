27e73b9912a680e8bd20e067169cece5e521dc3e
leelaur <laurenlee552@yahoo.com>
2022-10-06 20:51:00 -0400

    update

--- public_html/final/index.php
+++ public_html/final/index.php
@@ -1,4 +1,6 @@
 
 <?php include_once 'components/header.php';?>
-<h1> My IDM 232 Final </h1>
-<?php include 'components/footer.php';?>
+<?php $title = 'Assignment-01'; ?>
+<h1><?php echo $title; ?></h1>
+
+<?php include_once 'components/footer.php';?>
