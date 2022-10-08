14236bafe76fe499f69eb22fecc4732454ee3f40
leelaur <laurenlee552@yahoo.com>
2022-10-08 11:56:52 -0400

    space

--- public_html/final/index.php
+++ public_html/final/index.php
@@ -1,6 +1,6 @@
 
 <?php include_once 'components/header.php';?>
-<?php $title = 'Assignment-01'; ?>
+<?php $title = 'Assignment - 01'; ?>
 <h1><?php echo $title; ?></h1>
 
 <?php include_once 'components/footer.php';?>
