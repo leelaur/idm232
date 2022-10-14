5ab8468837486a44acb13e3e1348680691e757b5
leelaur <laurenlee552@yahoo.com>
2022-10-14 01:08:28 -0400

    Create Oct13-form.php

---
+++ public_html/class/Oct13-form.php
@@ -0,0 +1,36 @@
+<?php
+
+
+<form action= "" method= "">
+/* 'action' and 'method' is an attribute
+action is where you want to send it??
+method is how you want to send the data (send the data or receive?)
+*/
+?>
+
+<form action="" method="GET">
+    //sensitive information? DONT USE GET, use POST(safer)
+
+    <label for="firstName"> First Name </label>
+    // for= is this label for this input field
+    <input type="text" name="firstName" value="" placeholder=""> See all input types </label>
+    // name is like an id attribute 
+    // value is default value
+    // also look at attributes for input fields for more
+
+    <button onClick="someFunction()"> Reset </button>
+    //DO NOT USE BUTTON- not best practice
+
+    <input type="submit" value="you need this">
+
+</form>
+
+<?php 
+// ARRAYS
+$cars=[car1, car2, car3];
+
+// if unspecified, default will give value startig with 0
+
+redirect_to('https://www.google.com')
+// or can redirect inside files
+?>
