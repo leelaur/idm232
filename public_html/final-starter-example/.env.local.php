cce83ded35f18b8f1bb913db4b82eadb7a73641e
leelaur <laurenlee552@yahoo.com>
2022-11-03 20:20:52 -0400

    class 11/3

---
+++ public_html/final-starter-example/.env.local.php
@@ -0,0 +1,11 @@
+<?php
+// Specific to the current environment you're on.
+$APP_CONFIG = [
+    'environment' => 'development',
+    'site_name' => 'Jawn Clips',
+    'site_url' => 'http://localhost:8888/final',
+    'database_host' => 'localhost',
+    'database_user' => 'leelaurc_idm232',
+    'database_pass' => '#QO]UcMgb*+k',
+    'database_name' => 'local_idm232_01',
+];
