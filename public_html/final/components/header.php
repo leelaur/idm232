40cec8b279bdbd82e61c654a765df21362d6a551
leelaur <laurenlee552@yahoo.com>
2022-11-10 19:59:35 -0500

    class 11/10
    add and delete new files

--- public_html/final/components/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IDM 232 - Final</title>
-    <link rel="stylesheet" href="style.css">
-</head>
--- public_html/final/index.php
+++ public_html/final/index.php
@@ -1,31 +1,8 @@
-
-<?php include_once 'components/header.php';?>
-<?php $title = 'Assignment - 01'; ?>
-<h1><?php echo $title; ?></h1>
-
-<?php include_once 'components/footer.php';?>
-
 <?php
-  //  Read the data from the database form the users table
-  $query = 'SELECT * FROM users';
-  $result = mysqli_query($connection, $query);
+include_once __DIR__ . '/app.php';
+$page_title = 'Home';
+include_once __DIR__ . '/_components/header.php';
+?>
 
-  // Check there are no errors with our SQL statement
-  if (!$result) {
-      die('Database query failed.');
-  }
 
-  while ($user = mysqli_fetch_array($result)) {
-      // echo '<pre>';
-      // var_dump($user);
-      // echo '</pre>';
-      echo '<h2>' . $user['first_name'] . ' ' . $user['last_name'] . '</h2>';
-      echo '<hr>';
-  }
-  die;
-
-  $page_title = 'Home';
-  include_once 'components/header.php';
-
-  ?>
-
+<?php include_once __DIR__ . '/_components/footer.php';
