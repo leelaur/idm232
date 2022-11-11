61a188b9017bdbd58781aac222a30260bc76657d
leelaur <laurenlee552@yahoo.com>
2022-11-10 23:11:56 -0500

    add new html pages

--- public_html/main.js
+++ public_html/main.js
@@ -1,12 +1,2 @@
-const search=document.querySelector('#form');
-const searchText=document.querySelector('#searchInput');
-
-
-searchText.addEventListener("click", function(){
-
-    search.style.display="block";
-    search.classList('menu-list-item');
-    searchText.style.display="none";
-});
 
 
