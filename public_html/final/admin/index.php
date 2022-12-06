e225e15ff02684379967833422cdc709ed33237b
leelaur <laurenlee552@yahoo.com>
2022-12-06 15:17:55 -0500

    change redirect

--- public_html/final/admin/index.php
+++ public_html/final/admin/index.php
@@ -5,7 +5,7 @@ $page_title = 'Home';
 include_once __DIR__ . '/../_components/header.php';
 
 //redirect
-redirect_to('/admin/users.php');
+redirect_to('/admin/users');
 ?>
 <?php include_once __DIR__ . '/../_components/headspace.php';?>
 
