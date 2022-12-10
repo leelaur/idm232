43f03f756c4ecb4a3c71bffc6095792ecc3a6fbe
leelaur <laurenlee552@yahoo.com>
2022-12-10 10:59:56 -0500

    footer css

--- public_html/final/_components/footer.php
+++ public_html/final/_components/footer.php
@@ -2,7 +2,11 @@
 </main>
   <hr>
   <footer>
-        <p>  <a href="<?php echo site_url(); ?>/index.php" class="menu-link"> Unfolded Origami </a> </p>
+        <div class=footer>
+          <div class=footer-r>
+            <h2 > <a href="<?php echo site_url(); ?>/index.php" class="menu-link">Unfolded Origami </a> </h2>
+            <p class="copyright"> © 2022 IDM 232 All Rights Reserved. </p>
+          </div>
         <nav>
             <ul class="footer-menu-list">
                 <li class="footer-list-items"> <a href="<?php echo site_url(); ?>/index.php" class="menu-link">Search </a></li>
@@ -10,11 +14,13 @@
                 <li class=""> <a href="<?php echo site_url(); ?>/admin/index.php" class="menu-link">Admin</a></li>
             </ul>
         </nav>
-    </footer>
+</div>
 
+    </footer>
   <script src="<?php echo site_url(); ?>/dist/scripts/main.js">
 
 </script>
   </body>
 
   </html>
+
