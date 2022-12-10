f33578f72c28343861baf5327a0bd59315e67500
leelaur <laurenlee552@yahoo.com>
2022-12-10 11:47:15 -0500

    home css

--- public_html/final/index.php
+++ public_html/final/index.php
@@ -6,8 +6,16 @@ include_once __DIR__ . '/_components/header.php';
 
 <?php include_once __DIR__ . '/_components/headspace.php';?>
 
+
          <div class="search wrapper">
             <div class="search-img--center">
+                <div>
+
+            <h1 class="title"> Welcome to Unfolded Origami! </h1>
+            <p style="text-align:center;"> 
+                Learn and make origami from our 40+ instructions. Choose from our featured origami or choose a level of difficulty to get started.
+                 </p>
+            </div>
                 <img src="dist/images/search-img.png" class="search-img">
                 <form action="<?php echo site_url(); ?>/search" method="GET"class="search-bar" id="form"> 
                     <input type="search" placeholder="Search..." class="search-box"name="search">
@@ -16,18 +24,20 @@ include_once __DIR__ . '/_components/header.php';
             </div>
         </div>
 
+
+
+
         <?php
 // get users data from database
 $query = "SELECT * FROM instructions";
 $result = mysqli_query($db_connection, $query);
 ?>
-          
-
-        </div>
+<div style="position:static;"> 
+          <h2 class="subtitle"> Featured Origami </h2>
 <div class="all-origami">
 
         <?php include __DIR__ . '/_components/card.php'; ?>
 
 </div>
-
+</div>
 <?php include_once __DIR__ . '/_components/footer.php';
