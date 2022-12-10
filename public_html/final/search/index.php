d9639a6816938907d370139dc972c6a6b7b85d51
leelaur <laurenlee552@yahoo.com>
2022-12-10 14:04:20 -0500

    search results css

--- public_html/final/search/index.php
+++ public_html/final/search/index.php
@@ -37,11 +37,11 @@ if ($results->num_rows > 0) {
       <div class="sm:flex-auto">
       <?php include_once __DIR__ . '/../_components/headspace.php';?>
         <h1 class="text-xl font-semibold text-gray-900">Search Results</h1>
-        <form action="<?php echo site_url(); ?>/search" method="GET">
-          <input class=" border-black border-2" type="text" name="search" id="search" placeholder="Search"
-            value="<?php echo $search; ?>">
-          <button type="submit">Search</button>
-        </form>
+        <form action="<?php echo site_url(); ?>/search" method="GET"class="search-bar" id="form"> 
+                    <input type="search" placeholder="Search..." class="search-box"name="search"
+                    value="<?php echo $search; ?>">
+                    <button type="submit" class="search-button">Search</button>
+                </form>
         <h2>You searched for "<?php echo $search; ?>"</h2>
         <?php
         // If no results, echo no results
@@ -52,25 +52,24 @@ if ($results->num_rows > 0) {
         <?php
 // If error query param exist, show error message
   if (isset($_GET['error'])) {
-      echo '<p class="text-red-500">' . $_GET['error'] . '</p>';
-  }?>
+      echo '<p class="text-red-500">' . $_GET['error'] . '</p>';}
+      ?>
     </div>
 
     <?php
     // If we have results, show them
-      if ($instructions_results) {
-          while ($instructions_results = mysqli_fetch_assoc($results)) {
-              echo '<div class="flex flex-row justify-center items-center">';
-              echo '<h2>' . $instructions_results['title'] . ' ' . $instructions_results['description'] . '</h2>';
-              echo '</div>';
-          }
+    if ($instructions_results) {
+      while ($instructions_results = mysqli_fetch_assoc($results)) {
+          echo '<div class="flex flex-row justify-center items-center">';
+          echo '<h2>' . $instructions_results['title'] . ' ' . $instructions_results['description'] . '</h2>';
+          echo '</div>';
       }
-?>
+  }
+    ?>
 
   </div>
 </div>
 
 
 
-<?php include_once __DIR__ . '/../_components/footer.php';
-?>
+<?php include_once __DIR__ . '/../_components/footer.php';?>
