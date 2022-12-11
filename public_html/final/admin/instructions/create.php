7cbae50b5be5de2d273d948dd0d3878842356e6e
leelaur <laurenlee552@yahoo.com>
2022-12-11 14:20:46 -0500

    update css

--- public_html/final/admin/instructions/create.php
+++ public_html/final/admin/instructions/create.php
@@ -62,8 +62,8 @@ $result = mysqli_query($db_connection, $query);
               </div>
 
               <input
-                class="nline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto submit-btn"
-                type="submit" value="submit">
+                class="nline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto submit-input"
+                type="submit" value="Submit">
 
             </form>
           </div>
