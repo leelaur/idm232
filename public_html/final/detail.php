0dadec93875302f76f210ae9bf8f5c9e5c929ec8
leelaur <laurenlee552@yahoo.com>
2022-12-11 03:17:35 -0500

    search results

--- public_html/final/detail.php
+++ public_html/final/detail.php
@@ -24,19 +24,33 @@ $result = mysqli_query($db_connection, $query);
         echo "
                 <div class=''>
                 <div>
-                <h1 class='title '>{$instructions['title']}</h1>
+                <h1 class='title detail-title'>{$instructions['title']}</h1>
                 <p class='text-center'>Level: {$instructions['level']}</p>
   
             </div>
 
 
                     <div class='details-wrapper'>
-                        <p class='details-section'>{$instructions['description']}</p>
-                        <p class='details-section'><div class='details-subtitle'>Materials: <div>{$instructions['materials']}</p>
-                        <p class='details-section'><div class='details-subtitle'>Instructions:</div>{$instructions['instructions']}</p>
-                        <div class='details-img-container'>
+                        <p class='details-section'>
+                            {$instructions['description']}
+                        </p>
+                        <div class='details-section'>
+                            <p class='details-subtitle'>
+                                Materials:
+                            </p>
+                            <p>
+                                {$instructions['materials']}
+                            </p>
+                        </div>
+                        <div class='details-section'>
+                        <p class='details-subtitle'>
+                                Instructions:
+                            </p>
+                            <p class=''>
+                                {$instructions['instructions']}
+                            </p>
+                        </div>
                         <img class='details-img' width='500px' height='500px' src='{$site_url}/{$instructions['image']}' alt=''>
-            </div>
                     </div>
 
         ";
--- public_html/final/search/index.php
+++ public_html/final/search/index.php
@@ -31,17 +31,13 @@ if ($results->num_rows > 0) {
 
 ?>
 
-<div class="mx-auto my-16 max-w-7xl px-4">
-  <div class="px-4 sm:px-6 lg:px-8">
-    <div class="sm:flex sm:items-center">
-      <div class="sm:flex-auto">
       <?php include_once __DIR__ . '/../_components/headspace.php';?>
         <h1 class="text-xl font-semibold text-gray-900">Search Results</h1>
-        <form action="<?php echo site_url(); ?>/search" method="GET"class="search-bar" id="form"> 
-                    <input type="search" placeholder="Search..." class="search-box"name="search"
-                    value="<?php echo $search; ?>">
-                    <button type="submit" class="search-button">Search</button>
-                </form>
+        <form action="<?php echo site_url(); ?>/search" method="GET">
+          <input class=" border-black border-2" type="text" name="search" id="search" placeholder="Search"
+            value="<?php echo $search; ?>">
+          <button type="submit">Search</button>
+        </form>
         <h2>You searched for "<?php echo $search; ?>"</h2>
         <?php
         // If no results, echo no results
@@ -52,24 +48,37 @@ if ($results->num_rows > 0) {
         <?php
 // If error query param exist, show error message
   if (isset($_GET['error'])) {
-      echo '<p class="text-red-500">' . $_GET['error'] . '</p>';}
-      ?>
-    </div>
+      echo '<p class="text-red-500">' . $_GET['error'] . '</p>';
+  }?>
 
+
+<div class="all-origami">
     <?php
-    // If we have results, show them
-    if ($instructions_results) {
-      while ($instructions_results = mysqli_fetch_assoc($results)) {
-          echo '<div class="flex flex-row justify-center items-center">';
-          echo '<h2>' . $instructions_results['title'] . ' ' . $instructions_results['description'] . '</h2>';
-          echo '</div>';
-      }
-  }
-    ?>
+        $site_url = site_url();
 
-  </div>
+    // If we have results, show them
+
+      if ($instructions_results) {
+          while ($instructions_results = mysqli_fetch_assoc($results)) {
+            echo "
+            <a href='{$site_url}/detail.php?id={$instructions_results['id']}' class='instructions-link'>
+                <div class='cards all-origami'>
+                    <img class='cards-img' src='{$site_url}/{$instructions_results['image']}' alt=''>                
+                    <div class=''>
+                        <p class='title-txt'>{$instructions_results['title']}</p>
+                        <p class='level-txt'> Level: {$instructions_results['level']}</p>
+                    </div> 
+                </div>
+            </a>    
+                        ";
+          }
+      }
+?>
 </div>
 
 
 
-<?php include_once __DIR__ . '/../_components/footer.php';?>
+
+
+<?php include_once __DIR__ . '/../_components/footer.php';
+?>
